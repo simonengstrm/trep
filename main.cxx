f5a8765c348cee423cc3ea45fda1232a9a4edf7f
simonengstrm <simon_0_0_@live.se>
2024-12-21 17:04:36 +0100

    Better printing

--- main.cxx
+++ main.cxx
@@ -6,6 +6,8 @@
 #include <regex>
 #include <sstream>
 #include <string>
+#include <utility>
+#include <vector>
 
 #include "colors.hxx"
 
@@ -26,6 +28,36 @@ struct Command {
 struct Match {
     std::string line;
     int line_number;
+    std::vector<std::pair<int, int>> positions;
+
+    std::string to_string() const {
+        std::string highlight_line = line;
+        for (const auto &[start, end] : positions) {
+            highlight_line.replace(start, end - start,
+                                   YELLOW + line.substr(start, end - start) +
+                                       RESET);
+        }
+        return GREEN + std::to_string(line_number) + RESET + ": " +
+               highlight_line;
+    }
+};
+
+struct SearchResult {
+    const Command &cmd;
+    std::map<std::string, std::vector<Match>> matches;
+
+    std::string to_string() const {
+        std::stringstream ss;
+        for (const auto &[path, fileMatches] : matches) {
+            ss << MAGENTA << path << RESET << "\t" << std::endl;
+            for (const auto &match : fileMatches) {
+                ss << match.to_string() << std::endl;
+            }
+            ss << std::endl;
+        }
+
+        return ss.str();
+    }
 };
 
 std::optional<Command> parse_input(int argc, char *argv[]) {
@@ -69,46 +101,47 @@ std::vector<Match> search_file(const Command &cmd, const std::string &path) {
             line_number++;
             // Search for pattern in line
             // If found, add to fileMatches
-            if (std::regex_search(line, cmd.regex)) {
-                fileMatches.push_back({line, line_number});
+            std::smatch m;
+            if (std::regex_search(line, m, cmd.regex)) {
+                std::vector<std::pair<int, int>> positions;
+                for (const auto &sm : m) {
+                    positions.push_back(
+                        {sm.first - line.begin(), sm.second - line.begin()});
+                }
+                fileMatches.push_back({line, line_number, positions});
             }
         }
     }
-
     file.close();
     return fileMatches;
 }
 
-std::optional<std::map<std::string, std::vector<Match>>>
-search(const Command &cmd) {
-    std::map<std::string, std::vector<Match>> matches;
-
+std::optional<SearchResult> search(const Command &cmd) {
     // If cmd path is a file, search the file, else search all files in the
     // directory. Only perform search_file on path if it exists
-
-    // Check if path exists
-    if (std::filesystem::exists(cmd.path)) {
-        if (std::filesystem::is_regular_file(cmd.path)) {
-            // Search file
-            auto fileMatches = search_file(cmd, cmd.path);
-            matches[cmd.path] = fileMatches;
-        } else if (std::filesystem::is_directory(cmd.path)) {
-            // Search all files in directory
-            for (const auto &entry :
-                 std::filesystem::recursive_directory_iterator(cmd.path)) {
-                if (std::filesystem::is_regular_file(entry.path())) {
-                    auto fileMatches = search_file(cmd, entry.path());
-                    if (!fileMatches.empty())
-                        matches[entry.path()] = fileMatches;
-                }
-            }
-        }
-    } else {
+    if (!std::filesystem::exists(cmd.path)) {
         std::cerr << "Path does not exist: " << cmd.path << std::endl;
         return std::nullopt;
     }
 
-    return matches;
+    SearchResult searchResult{cmd};
+    if (std::filesystem::is_regular_file(cmd.path)) {
+        // Search file
+        auto fileMatches = search_file(cmd, cmd.path);
+        searchResult.matches[cmd.path] = fileMatches;
+    } else if (std::filesystem::is_directory(cmd.path)) {
+        // Search all files in directory
+        for (const auto &entry :
+             std::filesystem::recursive_directory_iterator(cmd.path)) {
+            if (std::filesystem::is_regular_file(entry.path())) {
+                auto fileMatches = search_file(cmd, entry.path());
+                if (!fileMatches.empty())
+                    searchResult.matches[entry.path()] = fileMatches;
+            }
+        }
+    }
+
+    return searchResult;
 }
 
 int main(int argc, char *argv[]) {
@@ -128,13 +161,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Print matches
-    for (const auto &[path, fileMatches] : matches.value()) {
-        cout << MAGENTA "File: " << RESET << path << "\t" << endl;
-        for (const auto &match : fileMatches) {
-            cout << GREEN << match.line_number << RESET << ": " << match.line
-                 << endl;
-        }
-        cout << endl;
-    }
+    cout << matches.value().to_string();
+
     return 0;
 }
